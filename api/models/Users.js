e7cac42b6155d981fa9a7dcd44d695df7ae33cc0
sslampa <sslampa@gmail.com>
2017-10-08 20:19:02 -0700

    Add User model to schema

---
+++ api/models/Users.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+  name: String,
+  email: String
+});
+
+module.exports = mongoose.model('User', UserSchema);
