8d2a11d0a9c98796942c3d69aa8f706806458805
Toma Nistor <toma.nistor@gmail.com>
2018-03-27 14:41:03 -0700

    Added webpack.LoaderOptionsPlugin in to minify CSS

--- webpack.config.js
+++ webpack.config.js
@@ -64,6 +64,9 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
     })
   ],
   devServer: {
